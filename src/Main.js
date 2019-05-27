248aa4e7bc337649db317f6c2267348d578a27b1
skyaie <skyaie@outlook.com>
2019-05-26 20:52:36 -0400

    Merge pull request

--- src/Main.js
+++ src/Main.js
@@ -5,8 +5,9 @@
 
 // React native and others libraries imports
 import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
 import { Root } from 'native-base';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 
 
 // Our custom files and classes import
@@ -20,15 +21,21 @@ import Contact from './page/Contact';
 import Category from './page/Category';
 import Product from './page/Product';
 import ImageGallery from './page/ImageGallery';
-
+import Login from './page/Login';
+import Signup from './page/Signup';
+import Checkout from './page/Checkout';
 
 export default class Main extends Component {
+  componentWillMount = () => {
+    BackHandler.addEventListener('hardwareBackPress', () => Actions.pop());
+  };
+
   render() {
     return(
       <Root>
         <Router>
           <Scene key="root">
-            <Scene initial={true} key="home" component={Home} hideNavBar />
+            <Scene initial key="home" component={Home} hideNavBar />
             <Scene key="search" component={Search} modal hideNavBar />
             <Scene key="cart" component={Cart} modal hideNavBar />
             <Scene key="wishlist" component={WishList} modal hideNavBar />
@@ -38,6 +45,9 @@ export default class Main extends Component {
             <Scene key="category" component={Category} hideNavBar />
             <Scene key="product" component={Product} hideNavBar />
             <Scene key="imageGallery" component={ImageGallery} modal hideNavBar />
+            <Scene key="login" component={Login} hideNavBar />
+            <Scene key="signup" component={Signup} hideNavBar />
+            <Scene key="checkout" component={Checkout} hideNavBar />
           </Scene>
         </Router>
       </Root>
